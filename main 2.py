b33d2b58900b4bd517807d54f2d040c314d1d229
AdamHorelican <horelicanadam@gmail.com>
2021-10-21 14:20:10 +0200

    adfs

---
+++ main 2.py
@@ -0,0 +1,195 @@
+
+#vek = int(input("zadaj svoj vek: "))
+#print( vek + 10)
+
+"""
+polomerKruhu = float(input("Zadaj pomoler kruhu: "))
+obvodKruhu = 2*3.14*polomerKruhu
+print("Obvod kruhu je ", obvodKruhu)
+"""
+
+"""
+obsahKruhu = float(input("Zadaj pomolomer: ", ))
+obsahKruhu = 3.14*polomerKruhu*polomerKruhu
+print("Obsah Kruhu je : ", obsahKruhu)"""
+
+"""
+a = float(input("Zadaj cislo a: "))
+b = float(input("Zadaj cislo b: "))
+if b == 0:
+    print("chyba s 0")
+else:
+   print("jahoda")
+   """
+
+""" 
++, -, ==, >, <, **, ->, !>, %, +   /
+"""
+
+"""
+vek = int(input("Zadaj svoj vek: "))
+if vek >=18:
+    print("Mas dost rokov")
+    print("tento prikaz sa vykona vzdy")
+else:
+    print("Nemas vela rokov")
+print("Toto sa vykona vzdy")
+"""
+"""
+x = 10
+y = 20
+
+tmp = x
+x = y
+y = tmp
+print(x, y)
+"""
+"""
+den = 5
+if den == 1:
+    print("Pondelok")
+elif den == 2:
+    print("Utorok")
+else: 
+    print("Nezadal si")
+"""
+
+"""
+stastny = input("si stastny? ")
+bohaty = input("si bohaty? ")
+
+if stastny == "ano":
+    if bohaty == "ano":
+        print("Gratulujem")
+    else:
+        print("Skus viac setrit")
+else:
+    if bohaty == "ano":
+        print("skus sa viac usmievat")
+    else:
+       print("Sorry je mi to luto")
+"""
+"""
+username = input("Zadaj prihlasovacie meno: ")
+password = input("Zadaj prihlasovacie heslo: ")
+vek = input("Zadaj prihlasovaci vek")
+
+if username == "root" and password == "password" and vek == "17":
+    print("Uspesne si sa prihlasil")
+else:
+    print("zle heslo alebo meno")
+"""
+"""
+
+start = 100
+koniec = 0
+while start >= koniec:
+    if start 
+
+    print(start)
+    start = start -5
+
+"""
+
+"""
+start = int(input("Zadaj zaciatocne cislo"))
+koniec = int(input("Zadaj koncove cislo"))
+
+while(start<koniec):
+    if(start % 3 ==0):
+        print(start)
+    start +=1
+"""
+
+"""
+slovo = input("Zadaj slovo: ")
+cisla = "1234567890"
+spoluhlasky = "bcčdďdzdžfghchjklľĺmnňpqrŕsštťvwxzž"
+samohlasky = "aeiouy"
+znaky = "()/!_:? "
+pocitadlo_samohlasike = 0
+pocitadlo_spoluhlasky = 0
+poci_cislo = 0
+pocidadlo_znaky = 0
+for znak in slovo:
+       if znak in samohlasky:
+         pocitadlo_samohlasike +=1
+       if znak in spoluhlasky:
+          pocitadlo_spoluhlasky +=1
+       if znak in cisla:
+            poci_cislo +=1
+       if znak in znaky:
+           pocidadlo_znaky +=1
+       
+
+
+
+if pocitadlo_samohlasike > 0:
+    print("obsahuje samohlasku", pocitadlo_samohlasike)
+else:
+    print("Neobsahuje samohlasky")
+if pocitadlo_spoluhlasky > 0:
+    print("Slovo ma spoluhlasok",pocitadlo_spoluhlasky)
+else:
+    print("nema spoluhlasky")
+if poci_cislo >0:
+    print("Slovo obsahuje", poci_cislo)
+else:
+    print("neobsahuje cisla")
+if pocidadlo_znaky >0:
+    print("slovo obsahuje",pocidadlo_znaky)
+else:
+    print("neobsahuje znaky")
+ """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
